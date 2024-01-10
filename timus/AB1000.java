3176aa8a019232058bc6eb3e196693d3fcbefb2d
Rexonis <140373571+Rexonis@users.noreply.github.com>
2024-01-10 19:28:19 +0500

    Add files via upload

---
+++ timus/AB1000.java
@@ -0,0 +1,20 @@
+package timus;
+
+import java.util.Scanner;
+
+public class AB1000 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int c;
+
+        c = a + b;
+
+        System.out.println(c);
+    }
+}
---
+++ timus/Enya1293.java
@@ -0,0 +1,22 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Enya1293 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        int result = (N*(A*B))*2;
+
+        out.println(result);
+        out.flush();
+
+    }
+}
